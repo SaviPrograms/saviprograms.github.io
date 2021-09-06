edfb7e8f64f9c90eb2b2fad446404a964d548585
SaviPrograms <SaviPrograms@users.noreply.github.com>
2021-09-06 05:16:15 +0000

    deploy: ea902c2d499cc55b10d611577877a0985569f66b

--- _nuxt/static/1630905261/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FSaviPrograms\u002F_nuxt\u002Fstatic\u002F1630905261",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002FSaviPrograms\u002F",assetsPath:"\u002FSaviPrograms\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8c868749"}}}}(null));
